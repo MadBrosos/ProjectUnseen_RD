41413112eaaa8fceb4746a5dd1c5cd8940ac885c
Armand <a.armar2003@gmail.com>
2024-11-04 11:35:47 +0100

    pleins de bordel

---
+++ ProjectUnseenRD/Source/ProjectUnseenRD/Public/StateTreeFunctionLibrary.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include  "StateTreeModule/Public/StateTreeReference.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "StateTreeFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTUNSEENRD_API UStateTreeFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
